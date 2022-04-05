69ccbd545cce8912aea2a3a5b11dfca1fb028097
Peter Chieng <pchieng3@gmail.com>
2022-04-04 20:02:47 -0500

    Part 1 (Server) Complete

--- db/index.js
+++ db/index.js
@@ -285,6 +285,17 @@ async function getPostsByTagName(tagName) {
     }
 }
 
+async function getAllTags() {
+    try {
+        const { rows } = await client.query(`
+        SELECT * FROM tags;
+        `)
+        return rows;
+    } catch(error) {
+        throw error;
+    }
+}
+
 
 
 module.exports = {
@@ -299,5 +310,6 @@ module.exports = {
     getPostsByUser,
     addTagsToPost,
     createTags,
-    getPostsByTagName
+    getPostsByTagName,
+    getAllTags
 }
