d63e246c11b829b71fe6279384e9f9e10a37550d
Peter Chieng <pchieng3@gmail.com>
2022-04-07 19:48:07 -0500

    Get tagName posts added

--- api/tags.js
+++ api/tags.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.use((req, res, next) => {
     console.log("A request is being made to /tags");
 
     next();
 });
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+    const { tagName } = req.params; 
+
+    try {
+        const posts = await getPostsByTagName(tagName);
+        res.send({ posts: posts });
+    } catch ({ name, message }) {
+        next ({ name, message });
+    }
+})
+
 
 tagsRouter.get('/', async (req, res, next) => {
     const tags = await getAllTags();
